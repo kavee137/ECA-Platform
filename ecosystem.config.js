2392345f22e68231d2dac4f2b5402e055246f9c3
R.Kaveesha Rukshan <kaveesharukshan137@gmail.com>
2026-03-29 17:38:20 +0530

    create logs

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps : [{
+    name   : "config-server",
+    script : "java -jar ./config-server/target/config-server-0.1.0-SNAPSHOT.jar",
+    log_file: "./logs/config-server.log",
+  },
+    {
+      name   : "eureka-server",
+      script : "java -jar ./eureka-server/target/eureka-server-0.1.0-SNAPSHOT.jar",
+      log_file: "./logs/eureka-server.log",
+    },
+    {
+      name   : "api-gateway",
+      script : "java -jar ./api-gateway/target/api-gateway-0.1.0-SNAPSHOT.jar",
+      log_file: "./logs/api-gateway.log",
+    }]
+}
